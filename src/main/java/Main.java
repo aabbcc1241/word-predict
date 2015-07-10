bbf1173f51cf0e627aae9519b3b8d65992083e8e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-07-11 02:10:21 +0800

    tested IDE settings

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import static myutils.debug.Debug.logd;
+
+/**
+ * Created by username on 7/11/15.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("main");
+        logd("end");
+    }
+}
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,11 @@
+import myutils.debug.Debug
+
+/**
+ * Created by username on 7/11/15.
+ */
+object Test extends App {
+  override def main(args: Array[String]) {
+    println("test")
+    Debug.logd("end")
+  }
+}
